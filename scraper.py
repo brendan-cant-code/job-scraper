44da3a570a240fb008b06cca546d28db889d9601
Brendan <brendandoesbusiness@gmail.com>
2026-07-27 18:50:34 -0400

    updated scraper.py file; updated smtp_user variable names to smtp_gmail_user

--- scraper.py
+++ scraper.py
@@ -166,7 +166,7 @@ def send_slack_notification(new_postings):
 def send_email_notification(new_postings):
     smtp_gmail_user = os.environ.get("SMTP_GMAIL_USER")
     smtp_gmail_pass = os.environ.get("SMTP_GMAIL_PASS")
-    notify_to = os.environ.get("NOTIFY_EMAIL")
+    notify_to = os.environ.get("NOTIFY_GMAIL_EMAIL")
     if not (smtp_gmail_user and smtp_gmail_pass and notify_to):
         return
 
